2c25693ba16b91139dd340887d4b98734032f4bf
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-06-18 08:59:39 +0530

    Refractor: initialize the discount

--- src/test/java/com/sdb/tdd/ShoppingCartTest.java
+++ src/test/java/com/sdb/tdd/ShoppingCartTest.java
@@ -4,12 +4,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import com.sdb.cart.ShoppingCart;
 import com.sdb.cart.SoftwareDevelopmentBook;
+import com.sdb.price.SoftwareDevelopmentBookSetDiscount;
+
 
 public class ShoppingCartTest {
 	ShoppingCart shoppingCart = new ShoppingCart();
+	
+	@BeforeEach
+	public void setup() {
+		List<SoftwareDevelopmentBookSetDiscount> byDifferentCopiesDiscountList = new ArrayList<>();
+
+		byDifferentCopiesDiscountList.add(new SoftwareDevelopmentBookSetDiscount(2, 5));
+		byDifferentCopiesDiscountList.add(new SoftwareDevelopmentBookSetDiscount(3, 10));
+		byDifferentCopiesDiscountList.add(new SoftwareDevelopmentBookSetDiscount(4, 20));
+		byDifferentCopiesDiscountList.add(new SoftwareDevelopmentBookSetDiscount(5, 25));	
+
+	}
 
 	@Test
 	public void buyingOneBook() {
